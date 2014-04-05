3345cca8dd6e2d7d23a1f10d48931885f36998c0
Ben Griffiths <benstephengriffiths@gmail.com>
2014-04-05 12:32:37 +0100

    initial commit

---
+++ ConsoleProxy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ConsoleProxy {
+
+    class Program {
+
+        static void Main(String[] args) {
+
+            if (args == null || args.Length < 1 || string.IsNullOrWhiteSpace(args[0])) {
+                Console.WriteLine("Please specify the name of the class whose _Main method should be invoked");
+            }
+            string typeName = string.Format("ConsoleProxy.{0}", args[0]);
+            if (args.Length > 1) {
+                args = args.Skip(1).ToArray();
+            }
+
+            Type type = Type.GetType(typeName);
+
+            type.GetMethod("_Main", BindingFlags.Static | BindingFlags.Public).Invoke(null, new object[] { args });
+
+        }
+
+    }
+}
